950dff8c01869e029007f38f913c0758ab86de0e
Al Chyrill Cabrera <alc@payrollpie.com>
2018-02-09 11:15:51 +0800

    initialize analytics

---
+++ src/modules/index.js
@@ -0,0 +1,8 @@
+export default app => {
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        res.header("Access-Control-Allow-Methods", "*");
+        next();
+    });
+};
